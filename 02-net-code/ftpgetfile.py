977d8d3caa82dba74c0471988bc104a7ff8f21fd
Evgeny Kudryavtsev <evgen.in.it@yandex.ru>
2023-02-10 18:10:50 +0700

    PEP 8

--- 02-net-code/ftpgetfile.py
+++ 02-net-code/ftpgetfile.py
@@ -1,15 +1,29 @@
+"""
+Загружает произвольный файл по FTP. Используется анонимный доступ к FTP, 
+если не указан кортеж user=(имя, пароль).
+"""
+
 from ftplib import FTP
+from os.path import exists
 
-adress = '185.54.136.70'
-userdata = () # анонимный доступ
-dirname = '/users/yura' # каталог, откуда загружаются файлы
-filename = 'readme!.txt'
+def ftp_get_file(file, address, dirname, user=()):
+    if exists(file): 
+        print(file, 'exists')
+    else:
+        localfile = open(file, 'wb')
+        try:
+            connection = FTP(address)
+            connection.login(*user)
+            connection.cwd(dirname)
+            connection.retrbinary('RETR ' + file, localfile.write, 1024) # RETR + filename - стандартный запрос загрузки файлов по FTP
+            connection.quit()
+        finally:
+            localfile.close()
 
-connection = FTP(adress)
-connection.login(userdata)
-connection.cwd(dirname)
+if __name__ == '__main__':
 
-localfile = open(filename, 'wb') 
-connection.retrbinary('RETR ' + filename, localfile.write, 1024) # RETR + filename - стандартный запрос загрузки файлов по FTP
-connection.quit()
-localfile.close()
+    address = '185.54.136.70'
+    dirname = '/users/yura' # каталог, откуда загружаются файлы
+    filename = 'readme!.txt' # загружаемый файл
+
+    ftp_get_file(filename, address, dirname)
