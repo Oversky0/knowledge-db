a18bfaa9959e7d3dee7720f53afbbb0d334f17d2
Evgeny Kudryavtsev <evgen.in.it@yandex.ru>
2023-02-17 21:05:18 +0700

    add class

--- 02-net-code/web-scraping/jobscraper.py
+++ 02-net-code/web-scraping/jobscraper.py
@@ -1,4 +1,5 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
 from bs4 import BeautifulSoup
 import re
 
@@ -7,14 +8,30 @@ import re
 # webword = re.compile(r"(парс)|(pars)|(скрап)|(scrap)")
 # pythonword = re.compile(r"\b(питон)|\b(python)")
 
-regExp = re.compile(r"(?i)\b(бот)|\b(bot)|(скрипт)|(script)|(телег)|(teleg)|(парс)|(pars)|(скрап)|(scrap)|\b(питон)|\b(python)")
+class WorkParser:
+    
+    def __init__(self, pages):
+        self.pages = pages
+        self.url = 'https://www.fl.ru/projects/?page={}&kind=5'
+        self.keyword = re.compile(r"(?i)\b(бот)|\b(bot)|(скрипт)|(script)|(телег)|(teleg)|(парс)|(pars)|(скрап)|(scrap)|\b(питон)|\b(python)")
+    
+    def parse(self):
+        for i in range(1, self.pages + 1):
+            try:
+                page = urlopen(self.url.format(i))
+            except HTTPError as err:
+                print('HTTP error: ', err)
+            except URLError as err:
+                print('URL error:', err)
 
-for i in range(1, 6):
-    page = urlopen('https://www.fl.ru/projects/?page={}&kind=5'.format(i))
-    soup = BeautifulSoup(page, 'html.parser')
+            soup = BeautifulSoup(page, 'html.parser')
+            
+            for head in soup.find('div', {'id': 'projects-list'}).find_all('h2'):
+                if head.find(string=self.keyword):
+                    print(head.get_text(), end='')
+                    print('https://www.fl.ru' + head.a['href'])
+                    print('=' * 80)
 
-    for head in soup.find('div', {'id': 'projects-list'}).find_all('h2'):
-        if head.find(string=regExp):
-            print(head.get_text(), end='')
-            print('https://www.fl.ru' + head.a['href'])
-            print('=' * 80)
+if __name__ == '__main__':
+    work = WorkParser(5)
+    work.parse()
